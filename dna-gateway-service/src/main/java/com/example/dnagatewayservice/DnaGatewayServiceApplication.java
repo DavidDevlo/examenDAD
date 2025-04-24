82a20520a6a75e7e356ad2e8221116a262379c2b
Edgar David Naira Alture <117549379+DavidDevlo@users.noreply.github.com>
2025-04-23 22:59:56 -0500

    Add files via upload
    va

---
+++ dna-gateway-service/src/main/java/com/example/dnagatewayservice/DnaGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnagatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnaGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ dna-gateway-service/src/test/java/com/example/dnagatewayservice/DnaGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnagatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
